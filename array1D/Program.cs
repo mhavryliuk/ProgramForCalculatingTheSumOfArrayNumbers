1224b27de989493d8b677c0b5fb7bdcb37820ca2
Maksym Havryliuk <maks.havryliuk@gmail.com>
2017-10-06 16:41:43 +0300

    Добавьте файлы проекта.

---
+++ array1D/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// A task:
+// Given a one-dimensional array of n integers.
+// Write a program to calculate the sum of these numbers.
+
+// Задача:
+// Дан одномерный массив из n целых чисел.
+// Написать программу для подсчета суммы этих чисел.
+
+namespace array1D
+{
+    class Program
+    {
+        static int[] Input()
+        {
+            Console.Write("Size of array n = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("a[{0}] = ", i);
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            return a;
+        }
+
+        static int Sum(int[] a)
+        {
+            int sum = 0;
+            foreach (int elem in a)
+            {
+                sum += elem;
+            }
+            return sum;
+        }
+
+        static void Main()
+        {
+            int[] a = Input();
+            Console.WriteLine("\nSum of array elements = {0}", Sum(a));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ array2D/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+// A task:
+// Given a two-dimensional array of n*m integers.
+// Write a program to calculate the sum of these numbers.
+
+// Задача:
+// Дан двумерный массив из n*m целых чисел.
+// Написать программу для подсчета суммы этих чисел.
+
+namespace array2D
+{
+    class Program
+    {
+        static int[,] Input()
+        {
+            Console.Write("Number of lines in the array n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Number of column in the array m = ");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] a = new int[n, m];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("a[{0},{1}] = ", i, j);
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return a;
+        }
+
+        static int Sum(int[,] a)
+        {
+            int sum = 0;
+            foreach (int elem in a)
+            { sum += elem; }
+            return sum;
+        }
+
+        static void Print(int[,] a)
+        {
+            Console.WriteLine("\nAvailable array:");
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main()
+        {
+            int[,] a = Input();
+            Print(a);
+            Console.WriteLine("\nSum of elements in a two-dimensional array = {0}", Sum(a));
+
+            Console.ReadKey();
+        }
+    }
+}
