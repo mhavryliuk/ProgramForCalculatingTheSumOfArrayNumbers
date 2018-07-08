47a44acb6a616f0bcce7209088f08182ab077159
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-07-09 00:32:15 +0300

    Refactored the code for arrau1D and added exception handling.

---
+++ array1D/OneDimensionalArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace array1D
+{
+    internal static class OneDimensionalArray
+    {
+        /// <summary>
+        /// The EnterArrayElements() method creates an array of user-entered numbers.
+        /// </summary>
+        /// <returns>The array created by the user.</returns>
+        internal static int[] EnterArrayElements()
+        {
+            Console.Write("Size of array n = ");
+            int.TryParse(Console.ReadLine() ?? throw new InvalidOperationException(), out int n);
+            int[] array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Element with index [{0}] = ", i);
+                array[i] = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            }
+
+            return array;
+        }
+
+        /// <summary>
+        /// The AddArrayNumbers() method calculates the sum of the numbers in the array.
+        /// </summary>
+        /// <param name="array">A one-dimensional array of n integers.</param>
+        /// <returns>The sum of the numbers of the given array.</returns>
+        internal static int AddArrayNumbers(int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            if (array.Length == 0)
+                throw new ArgumentException("Value cannot be an empty collection.", nameof(array));
+
+            return array.Sum();
+        }
+    }
+}
--- array1D/Program.cs
+++ array1D/Program.cs
@@ -1,44 +1,41 @@
 ﻿using System;
 
-// A task:
-// Given a one-dimensional array of n integers.
-// Write a program to calculate the sum of these numbers.
-
-// Задача:
-// Дан одномерный массив из n целых чисел.
-// Написать программу для подсчета суммы этих чисел.
+/**<remark>
+ * Given a one-dimensional array of n integers.
+ * Write a program to calculate the sum of these numbers.
+ </remark> */
 
 namespace array1D
 {
-    class Program
+    internal class Program
     {
-        static int[] Input()
+        private static void Main()
         {
-            Console.Write("Size of array n = ");
-            int n = int.Parse(Console.ReadLine());
-            int[] a = new int[n];
-            for (int i = 0; i < a.Length; i++)
+            try
             {
-                Console.Write("a[{0}] = ", i);
-                a[i] = int.Parse(Console.ReadLine());
+                int[] array = OneDimensionalArray.EnterArrayElements();
+                Console.WriteLine("\nSum of array elements = {0}", OneDimensionalArray.AddArrayNumbers(array));
             }
-            return a;
-        }
-
-        static int Sum(int[] a)
-        {
-            int sum = 0;
-            foreach (int elem in a)
+            catch (ArgumentNullException ex)
             {
-                sum += elem;
+                Console.WriteLine(ex.Message);
             }
-            return sum;
-        }
-
-        static void Main()
-        {
-            int[] a = Input();
-            Console.WriteLine("\nSum of array elements = {0}", Sum(a));
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Write("\nPress any key to exit...");
+            }
 
             Console.ReadKey();
         }
